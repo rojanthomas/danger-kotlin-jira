6cfc1efc1f0853bc224cf56cf69ef612d93b8682
Rojan Thomas <rojan.thomas@bglgroup.co.uk>
2021-07-02 15:36:52 +0100

    Tests + Html improvements

--- src/main/kotlin/com/rojanthomas/dangerkotlinjira/JiraPlugin.kt
+++ src/main/kotlin/com/rojanthomas/dangerkotlinjira/JiraPlugin.kt
@@ -4,22 +4,33 @@ import systems.danger.kotlin.sdk.DangerPlugin
 
 object JiraPlugin : DangerPlugin() {
 
-    private val jiraIssuesToHtmlMapper = JiraIssuesToHtmlMapper()
+    private val jiraIssuesToHtmlMapper = JiraIssuesToHtmlMapper(
+        JiraIssueCollector(
+        )
+    )
 
     override val id: String
         get() = this.javaClass.name
 
+    /**
+     * @param jiraUrl the full base JIRA url e.g. https://myjira.atlassian.net/
+     * @param projectKeys a set of project keys e.g setOf("ABCD", "EFG"). The order of this list will determine the order of the JIRA
+     * keys in the Danger comment.
+     * @param sources a list of strings to scan for JIRA keys.
+     */
     fun check(
         jiraUrl: String,
-        projectKeys: Set<String>,
-        sources: Set<String>,
+        projectKeys: List<String>,
+        sources: List<String>,
     ) {
         context.message(
             message = jiraIssuesToHtmlMapper.map(
                 jiraUrl = jiraUrl,
                 projectKeys = projectKeys,
                 sources = sources
-            )
+            ).also {
+                println("Message: $it")
+            }
         )
     }
 }
--- src/test/kotlin/com/rojanthomas/dangerkotlinjira/JiraIssueRegexFactoryTest.kt
+++
@@ -1,5 +0,0 @@
-package com.rojanthomas.dangerkotlinjira
-
-import org.junit.jupiter.api.Assertions.*
-
-internal class JiraIssueRegexFactoryTest
