ef380a1cbfff3ccc2680ccc06fbc62b10ba8e70c
Rojan Thomas <rojanthomas@gmail.com>
2021-07-21 10:44:31 +0100

    Improving test

--- src/test/kotlin/com/rojanthomas/dangerkotlinjira/JiraPluginTest.kt
+++ src/test/kotlin/com/rojanthomas/dangerkotlinjira/JiraPluginTest.kt
@@ -27,19 +27,23 @@ internal class JiraPluginTest {
         clearAllMocks()
     }
 
-    @Disabled
     @Test
-    fun test() {
+    fun `given a jiraUrl, multiple project keys & sources, when check is invoked, then return the correct message`() {
         JiraPlugin.check(
             "https://comparethemarket.atlassian.net/browse/",
-            projectKeys = listOf("MA", "TA"),
+            projectKeys = listOf("PA", "DKJ"),
             sources = listOf(
-                "hello",
-                "this is a jira issue MA-4458",
-                "this is a jira issue TA-4451, TA-4450"
+                "PR title with JIRA issue PA-123",
+                "this is a jira issue PA-456",
+                "this is a jira issue PA-789, DKJ-123",
+                "this is text with unrelated JIRA issues referenced CA-123",
+                "this is text with no JIRA issues referenced"
             )
         )
 
-        verify { context.message("<a href='https://comparethemarket.atlassian.net/browse/MA-4458'>MA-4458</a>") }
+        verify { context.message("<h2>JIRA issues</h2><ul><li><a href=\"https://comparethemarket.atlassian.net/browse" +
+                "/PA-123\">PA-123</a></li><li><a href=\"https://comparethemarket.atlassian.net/browse/PA-456\">PA-456" +
+                "</a></li><li><a href=\"https://comparethemarket.atlassian.net/browse/PA-789\">PA-789</a></li><li><a " +
+                "href=\"https://comparethemarket.atlassian.net/browse/DKJ-123\">DKJ-123</a></li></ul>") }
     }
 }
